fbcbd8d7ed653b9b48b950bff9c34764e7cdbea4
rushtehrani <r@onepanel.io>
2020-06-06 21:48:34 -0700

    remove top_bar from vnc lite

--- src/common/install/no_vnc.sh
+++ src/common/install/no_vnc.sh
@@ -9,5 +9,7 @@ wget -qO- https://github.com/novnc/noVNC/archive/v1.1.0.tar.gz | tar xz --strip 
 # use older version of websockify to prevent hanging connections on offline containers, see https://github.com/ConSol/docker-headless-vnc-container/issues/50
 wget -qO- https://github.com/novnc/websockify/archive/v0.9.0.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME/utils/websockify
 chmod +x -v $NO_VNC_HOME/utils/*.sh
-## create index.html to forward automatically to `vnc_lite.html`
+# create index.html to forward automatically to `vnc_lite.html`
 ln -s $NO_VNC_HOME/vnc_lite.html $NO_VNC_HOME/index.html
+# hide #top_bar
+sed -i '/#top_bar/{n;s/.*/\t\tdisplay: none;/}' $NO_VNC_HOME/vnc_lite.html
